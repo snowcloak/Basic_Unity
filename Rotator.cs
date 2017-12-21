148ff9fc9de3b30a79cecb8101d54ca089b048ef
Miguel Zeng <migzeng27@gmail.com>
2017-12-20 22:31:01 -0500

    Add files via upload

---
+++ Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed_X;
+	public float speed_Y;
+	public float speed_Z;
+
+	// Update is called once per frame
+	void Update () {
+	
+		//rotates along the X-axis
+		transform.Rotate (new Vector3 (speed_X, speed_Y, speed_Z) * Time.deltaTime);
+
+	}
+}
